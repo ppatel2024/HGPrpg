9f29970b2ba4983cefe29f85762bfdbe904c9e7a
mkoptyakov <matthewkoptyakov@gmail.com>
2021-12-09 12:03:10 -0500

    Final Boss class created

---
+++ src/FinalBoss.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class FinalBoss {
+
+
+
+        FinalBoss() {
+
+        }
+
+        //Helper Methods
+        void fighttheBoss(Hero atom, Enemy dog) {
+            boolean FinalBattle = true;
+            Scanner bScan = new Scanner(System.in);
+            int userInput;
+
+            while (FinalBattle) {
+
+}
