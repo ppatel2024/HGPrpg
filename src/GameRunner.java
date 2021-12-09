1cd948da5a9a001745e4ff275f690af18571e5e8
mkoptyakov <matthewkoptyakov@gmail.com>
2021-12-08 22:58:53 -0500

    Enemies Given Health and Attack Power

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -7,15 +7,27 @@ public class GameRunner {
         Enemy fly = new Enemy();
         //Health 5, Attack 5,
         //25% chance of spawn, 10% chance of item drop, 90% chance of dropping coin (1)
+        fly.setHealth(5);
+        fly.setAttackPower(5);
+
         Enemy rat = new Enemy();
         //Health 10, Attack 5,
         //25% chance of spawn, 20% chance of item drop, 80% chance of dropping coins (2)
+        rat.setHealth(10);
+        rat.setAttackPower(5);
+
         Enemy spider = new Enemy();
         //Health 20, Attack 10,
         //10% chance of spawn, 30% chance of item drop, 70% chance of dropping coins (3)
+        spider.setHealth(20);
+        spider.setAttackPower(10);
+
         Enemy dog = new Enemy();
         //FINAL BOSS
         //Health 100, Attack 30
+        dog.setHealth(100);
+        dog.setAttackPower(30);
+
         World w = new World();
         Scanner scan = new Scanner(System.in);
         boolean gameOn = true;
